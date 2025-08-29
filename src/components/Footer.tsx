76b1cbe1aa2faeabdf1017ad2b570a2d513aed28
Skandeerkefi <skanderkefi88@gmail.com>
2025-08-29 20:14:45 +0100

    WIP: Save progress before merging into master

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,7 @@ export function Footer() {
 						<div className='grid grid-cols-2 gap-2'>
 							{[
 								{ to: "/", label: "Home" },
-								{ to: "/leaderboard", label: "Leaderboard" },
+								{ to: "/leaderboards", label: "Leaderboard" },
 								{ to: "/terms", label: "Terms & Conditions" },
 								{ to: "/privacy", label: "Privacy Policy" },
 							].map(({ to, label }) => (
