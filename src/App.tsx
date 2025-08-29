91f11b73e2a55665302d1aabc037e57b97d02c70
Skandeerkefi <skanderkefi88@gmail.com>
2025-08-29 20:07:07 +0100

    WIP: Save progress before merging into master

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
-import LeaderboardPage from "@/pages/LeaderboardPage";
+// import LeaderboardPage from "@/pages/LeaderboardPage";
 import SlotCallsPage from "@/pages/SlotCallsPage";
 import GiveawaysPage from "@/pages/GiveawaysPage";
 import LoginPage from "@/pages/LoginPage";
@@ -35,7 +35,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<HomePage />} />
-					<Route path='/leaderboard' element={<LeaderboardPage />} />
+					{/* <Route path='/leaderboard' element={<LeaderboardPage />} /> */}
 					<Route path='/slot-calls' element={<SlotCallsPage />} />
 					<Route path='/giveaways' element={<GiveawaysPage />} />
 					<Route path='/login' element={<LoginPage />} />
